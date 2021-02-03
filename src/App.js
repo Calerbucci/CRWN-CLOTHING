e2ebd286fde125e5c7de4db94d3360c5d6a403ed
calerbucci <calerblj@gmail.com>
2021-02-04 03:47:49 +0800

    Router added to app using to React router and withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/Homepage'
+import {Switch, Route, withRouter } from "react-router-dom";
+
+const HatsPage = () => (
+    <div>
+        <h1> Hats Page</h1>
+    </div>
+)
+
 
 function App() {
   return (
     <div >
-      <Homepage/>
+      {/* <Homepage/> */}
+
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route path='/shop/hats' component={HatsPage}/>
+      </Switch>
+
+      
     </div>
   );
 }
--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -47,7 +47,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
             {
-                this.state.sections.map(({title, imageUrl, id, size}) => ( <Menu key ={id} title={title} imageUrl={imageUrl} size={size}/>))
+                this.state.sections.map(({title, imageUrl, id, size, linkUrl}) => ( <Menu key ={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>))
+                
             }
             
             </div>
